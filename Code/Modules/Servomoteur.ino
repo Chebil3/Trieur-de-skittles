58b927c058e57baef0c2a0a90d8aed27a9f241b2
Chebil3 <45876574+Chebil3@users.noreply.github.com>
2019-03-13 00:15:38 +0100

    Update Servomoteur.ino

--- Code/Modules/Servomoteur.ino
+++ Code/Modules/Servomoteur.ino
@@ -1,16 +1,16 @@
 #include<Servo.h>
-Servo servo1;
+Servo servo1;              //Declaration des deux objets servo
 Servo servo2;
 
 void PositiveTurn(int beginning,int last,int speedd, Servo moteur){
-   for (int i=beginning; i<=last; i++){                           
+   for (int i=beginning; i<=last; i++){                                 //cette rotation concerne une augmentation d'angle
       moteur.write(i);
       Serial.print("A");
       Serial.println(i);
-      delay(speedd);}
+      delay(speedd);}                                                   //ici on définit la vitesse angulaire du moteur
 }
 void NegativeTurn(int beginning,int last,int speedd, Servo moteur){
-   for (int i=beginning; i>=last; i--){                           
+   for (int i=beginning; i>=last; i--){                                 //cette rotation concerne une diminution d'angle                  
       moteur.write(i);
       Serial.print("B");
       Serial.println(i);
